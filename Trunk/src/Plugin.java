63b188b9974ab1ab391e1bc135613be91311f5c9
deathbob <deathbob@23f83f56-a308-4d86-8998-a9fc3f2562b2>
2008-01-12 06:23:10 +0000

    Major work done to Plugin system.  New method of handling the threads for it.  Breaks plugins and some of the plugin manager dialog.
    git-svn-id: http://thebes.codingwell.net/repos/BC@44 23f83f56-a308-4d86-8998-a9fc3f2562b2

--- Trunk/src/Plugin.java
+++ Trunk/src/Plugin.java
@@ -7,6 +7,7 @@
  */
 
 import javax.swing.*;
+import java.util.*;
 
 /**
  * This is the abstract class for any v1 Plugin.
@@ -14,16 +15,19 @@ import javax.swing.*;
  * This API is changing rapidly in pre-1.0 stages and may break Plugins. 
  *
  * @author Deathbob 
- * @version 0.2.5 2007/12/28
+ * @version 0.3.0 2007/12/28
  */
 
 abstract public class Plugin
 {
 	//CONSTS
 	
-	public static enum state {Initilizing, Running, Updating, Stopping, Stopped, Removing};
+	/**
+	 * The run state the plugin is in.
+	 */
+	public static enum state {Initilizing, Running, Stopping, Stopped, Removing};
 	
-	private static enum norunReason {Removal, Update};
+	private static enum norunReason {Removal, Reload};
 	
 	//
 	//PRIVATE
@@ -34,33 +38,97 @@ abstract public class Plugin
 	
 	//Locks  //Create a REALLY SMALL but unique object.
 	private final Object lock_core = new Object(); //Locks whenever we want to change something relating to cores.
+	private final Object lock_coreManager = new Object(); //Locks whenever we do something with whether the core manger is running.
 	
 	private volatile state currentState = state.Stopped;
 	
 	//Variables
 	
-	/**
-	 * True: The Plugin is currently running in some shape or form.
-	 * False: All operation has ceased.
-	 */
-	private boolean running = false;
+	private List<Thread> threadList = Collections.synchronizedList(new LinkedList<Thread>());
 	
-	/**
-	 * True: The Plugin is currently performing an update.
-	 * False: The Plugin is not performing an update.
-	 */
-	private boolean updating = false;
-	
-	/**
-	 * True: The Plugin is stopping whatever it is doing as soon as it safely can.
-	 * False: The Plugin is running or stopped.
-	 */
-	private boolean stop = false;
-	
+	private volatile long threadCount = 0;
 
+	private volatile boolean coreManagerRunning = false;
+	
 	//Functions
 	
 	//Classes
+	
+	private final class coreManager extends Thread
+	{
+		@Override
+		public void run()
+		{
+			while(true)
+			{
+				synchronized(lock_core)
+				{
+					if(threadList.size() < threadCount)
+					{
+						currentState = state.Initilizing;
+						coreRunner cr = new coreRunner(threadList.size()==0);
+						threadList.add(cr);
+						cr.setPriority(Thread.MIN_PRIORITY);
+						cr.start();
+						currentState = state.Running;
+					}
+				
+				
+					Iterator<Thread> it = threadList.iterator();
+					while(it.hasNext())
+					{
+						Thread th = it.next();
+						if(!th.isAlive()) it.remove();
+					}
+				}
+				
+				synchronized(lock_coreManager)
+				{
+					if(threadCount == 0)
+					{
+						if(threadList.size() == 0)
+						{
+							currentState = state.Stopped;
+							coreManagerRunning = false;
+							return;
+						}
+						currentState = state.Stopping;
+					}
+				}
+				
+				try
+		    	{
+		    		Thread.sleep(500);
+		    	}
+		    	catch(InterruptedException e)
+		    	{ 	}
+			}
+		}
+	}
+	
+	private final class coreRunner extends Thread
+	{
+		private volatile boolean main = false;
+		
+		public coreRunner(boolean main)
+		{
+			this.main = main;
+		}
+		
+		@Override
+		public void run()
+		{
+			if(main)
+			{
+				currentState = state.Running;
+				main();
+			}
+			else
+			{
+				core();
+			}
+		}
+	}
 	
 	private final class noRun
 	{
@@ -139,14 +207,6 @@ abstract public class Plugin
      */
     protected void core() {return;}
     
-    /**
-     * Main Update Method of the Plugin.
-     * Note: Calling this directly generally defeats the purpose of BackComp calling it.
-     * 
-     * @return Whether the Plugin needs to be reloaded because something changed.
-     */
-	protected boolean update(){return false;}
-    
 	/**
 	 * Main Removal Method. This is called when the plugin should uninstall itself.
 	 * Note: A java class can tamper with its own file while it is running.
@@ -154,14 +214,29 @@ abstract public class Plugin
 	abstract protected void remove();
 	
 	/**
-	 * Called by the plugin to start the update function after all cores have been terminated.
+	 * Called by the plugin to force reload of Plugin.
 	 */
-	protected final void startUpdate()
+	protected final void reload()
 	{
-		norun.setFlag(norunReason.Update);
-		norun.clearFlag(norunReason.Update);
-		//FIXME: Not thread safe.
-		boolean result = update();
+		norun.setFlag(norunReason.Reload);
+		
+		stopAll(true);
+		
+		BC.PError("Attempt to reload Plugin; Not Implemented"); return;
+		//TODO: Reload Code
+	}
+	
+	protected final boolean currentCoreShouldExit()
+	{
+		Thread ct = Thread.currentThread();
+		synchronized(lock_core)
+		{
+			if(threadList.size() > threadCount && threadList.lastIndexOf(ct) == threadList.size()-1)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	/**
@@ -225,7 +300,7 @@ abstract public class Plugin
      */
     public final long getRunningCores()
     {
-    	return 0;
+    	return threadCount;
     }
     
     /**
@@ -238,38 +313,50 @@ abstract public class Plugin
     	synchronized(lock_core)
 		{
     		if(!norun.isOK() || !needCore()) return false;
-    		//Create if need
+
+    		threadCount += 1;
     		
-    		if(false /*need to start main?*/)
-    		{
-    			currentState = state.Initilizing;
-    			
-    		}
+    		synchronized(lock_coreManager)
+			{
+    			if(coreManagerRunning == false)
+    			{
+    				coreManager cm = new coreManager();
+    				cm.start();
+    				coreManagerRunning = true;
+    			}
+			} 		
 		}
     	return true;
     }
     
     /**
-     * Stops 1 running core. If none are running it does nothing.
+     * Stops 1 running core at the Plugin's convienience. If none are running it does nothing.
      */
     public final void stopCore(boolean includeMain)
     {
     	synchronized(lock_core)
 		{
-    		//TODO
+    		if(threadCount > ( includeMain ? 0 : 1 ) )
+    			threadCount -= 1;
 		}
     }
     
     /**
-     * Stops all running cores. If none are running it does nothing.
+     * Stops all running cores at the Plugin's convienience. If none are running it does nothing.
      */
     public final void stopAll(boolean includeMain)
     {
     	synchronized(lock_core)
 		{
-    		currentState = state.Stopping;
-    		//TODO: Actually Stop
-    		currentState = state.Stopped;
+    		if(includeMain && threadCount > 0)
+    		{
+    			currentState = state.Stopping;
+    			threadCount = 0;
+    		}
+    		else if(!includeMain && threadCount > 1)
+    		{
+    			threadCount = 1;
+    		}
 		}
     }
     
@@ -283,9 +370,18 @@ abstract public class Plugin
     		norun.setFlag(norunReason.Removal); //Stop all new core operations.
 		}
     	
+    	currentState = state.Removing;
+    	
     	stopAll(true); //Stop All Cores
     	
-    	remove(); //Call Plugins Removal Method
+    	//FIXME: Check all cores are stopped. Size of threadList?
+    	
+    	currentState = state.Removing;
+    	
+    	BC.PError("Tried to remove a plugin; Not Implemented");
+    	return;
+    	
+    	//remove(); //Call Plugins Removal Method
     	
     	//TODO: Remove from Plugin Cache and do garbage collection multiple times.
     }
--- Trunk/src/Worker.java
+++ Trunk/src/Worker.java
@@ -4,48 +4,123 @@
  * Background Compute ( Manages Distributed Projects )
  * Copyright (C) 2007 Thomas Suckow (Deathbob)
  *
- * @author 
+ * @author Deathbob
  * @version 1.00 2007/2/10
  */
- 
-import java.util.*;
+
+/**
+ * This is the class for managing Plugins.
+ *  
+ *
+ * @author Deathbob 
+ * @version 0.2.0 2008/1/11
+ */
 
 public class Worker extends Thread
 {	
-	ArrayList<WorkRunner> threads = new ArrayList<WorkRunner>();
-
     public void run()
-    {
+    { 	
+    	long coreLevelMax = 0;//Max's
+    	long coreLevelMin = Long.MAX_VALUE;
+    	
     	while(true)
     	{
-    		try
+    		long IcoreLevelMax = 0;
+        	long IcoreLevelMin = 0;
+        	long coreTotal = 0;
+    		boolean didsomething = false;
+    		
+	    	int maxthreads = Runtime.getRuntime().availableProcessors();
+	    	
+	    	String[] Plugins = Utils.getLocalPlugins();
+	    	for(int i = 0; i < Plugins.length; ++i)
 	    	{
-	    		Thread.sleep(1000);
+	    		Plugin plug = Utils.loadPlugin(Plugins[i]);
+	    		if(plug != null)
+	    		{
+	    			long cores = plug.getRunningCores();
+	    			
+	    			if(!didsomething && (coreLevelMax != coreLevelMin+1 && coreLevelMax != coreLevelMin) && cores == coreLevelMax && cores > 0)//Max Exceeded
+	    			{
+	    				System.out.println("Stopped out of ballance core on plugin: " + plug.getName());
+	    				plug.stopCore(false);
+	    				cores = plug.getRunningCores();
+	    				didsomething = true;
+	    			}
+	    			if(!didsomething && cores == coreLevelMax && coreTotal > maxthreads)//Too many cores
+	    			{
+	    				System.out.println("Stopped system exceeding core on plugin: " + plug.getName());
+	    				plug.stopCore(true);
+	    				cores = plug.getRunningCores();
+	    				didsomething = true;
+	    			}
+	    			if(!didsomething && coreTotal < maxthreads && cores == coreLevelMin)
+	    			{
+	    				System.out.println("Started core on plugin: " + plug.getName());
+	    				plug.startCore();
+	    				cores = plug.getRunningCores();
+	    				didsomething = true;
+	    			}
+	    			
+	    			//Update the stats
+	    			coreTotal += cores;
+	    			if(cores < IcoreLevelMin)//Min Search
+	    			{
+	    				IcoreLevelMin = cores;
+	    			}
+	    			if(cores > IcoreLevelMax)//Max Search
+	    			{
+	    				IcoreLevelMax = cores;
+	    			}
+	    		}
+	    	}
+    	
+	    	coreLevelMax = IcoreLevelMax;
+	    	coreLevelMin = IcoreLevelMin;
+/*	    	
+	    	if(!didsomething && coreCount < maxthreads)
+	    	{
+	    		coreLevel++;
+	    	}
+	    		
+	    	didsomething = false;
+	    	
+	    	
+	    	
+	    	String[] Plugins = Utils.getLocalPlugins();
+	    	for(int i = 0; i < Plugins.length; ++i)
+	    	{
+	    		Plugin plug = Utils.loadPlugin(Plugins[i]);
+	    		if(plug.getState() == Plugin.state.Stopped)
+	    		{
+	    				System.out.println("Started new plugin: " + plug.getName());
+	    				plug.startCore();
+	    				didsomething = true;
+	    			}//~If
+	    			else if(!didsomething && coreOK)
+	    			{
+	    				System.out.println("Started new core: " + plug.getName());
+	    				plug.core();
+	    				didsomething = true;
+	    			}
+	    				
+	    			try
+			    	{
+			    		Thread.sleep(3000);//TODO: Lower this number.
+			    	}
+			    	catch(InterruptedException e)
+			    	{ 	}
+	    			Plugins = Utils.getLocalPlugins();//Refresh Plugin List
+	    			
+	    		}//~For
+*/
+	    	
+	    	try
+	    	{
+	    		Thread.sleep(2000);
 	    	}
 	    	catch(InterruptedException e)
 	    	{ 	}
-	    	
-	    	int maxthreads = Runtime.getRuntime().availableProcessors();
-	    	
-	    	if(maxthreads > threads.size())
-	    	{
-	    		WorkRunner runner = new WorkRunner();
-	    		runner.setPriority(Thread.MIN_PRIORITY);
-	    		System.out.println("Starting new worker thread");
-	    		runner.start();
-	    		threads.add(runner);
-	    	}
-	    	
-	    	if(threads.size() > maxthreads)
-	    	{
-	    		threads.get(0).end();
-	    		try
-		    	{
-		    		Thread.sleep(2000);
-		    	}
-		    	catch(InterruptedException e)
-		    	{ 	}
-	    	}
     	}
     	
     }
