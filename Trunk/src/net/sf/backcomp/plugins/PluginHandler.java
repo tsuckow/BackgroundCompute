e28cec03018dcda592cca2ff33b35a2b2dedae73
deathbob <deathbob@23f83f56-a308-4d86-8998-a9fc3f2562b2>
2008-11-10 07:23:40 +0000

    Nothing was wrong, plugin problem Note: Debug window needs work.
    git-svn-id: http://thebes.codingwell.net/repos/BC@245 23f83f56-a308-4d86-8998-a9fc3f2562b2

--- Trunk/src/net/sf/backcomp/plugins/PluginHandler.java
+++ Trunk/src/net/sf/backcomp/plugins/PluginHandler.java
@@ -62,7 +62,7 @@ public class PluginHandler
 		}
 		catch(NoClassDefFoundError ex)
 		{
-			Debug.messageDlg("Class Definition not found: " + "plugins/" + pluginName + "/" + pluginName + Debug.getStackTrace(ex),DebugLevel.Error,ex);
+			Debug.message("Class Definition not found: " + "plugins/" + pluginName + "/" + pluginName,DebugLevel.Error,ex);
 			return;
 		}
 		
@@ -90,7 +90,7 @@ public class PluginHandler
 		}
 		catch(NoClassDefFoundError ex)
 		{
-			Debug.messageDlg("Class Def Missing: " + pluginName + Debug.getStackTrace(ex),DebugLevel.Error,ex);
+			Debug.message("Class Def Missing: " + pluginName,DebugLevel.Error,ex);
 			return;
 		}
 		
